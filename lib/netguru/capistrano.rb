8ab6381aa5334f4c5e3210c2669f69001f419b41
Marcin Stecki <madsheeppl@gmail.com>
2011-12-10 12:23:33 +0100

    rip-off air code, specs initial idea

--- lib/netguru/capistrano.rb
+++ lib/netguru/capistrano.rb
@@ -1,22 +1,34 @@
 # Defines netguru custom task to deploy project.
 
-Capistrano::Configuration.instance(:must_exist).load do
+require 'capistrano'
 
-  set(:ng_conf) { fetch(:netguru_use) || [:secondcoder, :notify_airbrake] }  
+module Netguru
+  module Capistrano
+    def self.load_into(configuration)
+      configuration.load do
 
-  #check secondcoder
-  before "deploy:update_code" do
-    if ng_conf.include?(:secondcoder)
-      standup_response = open("http://secondcoder.com/api/netguru/#{application}/check").read
-      raise "Computer says no!\n#{standup_response}" unless standup_response == "OK"
+       set(:ng_conf) { fetch(:netguru_use) || [:secondcoder, :notify_airbrake] }  
+
+       #check secondcoder
+       before "deploy:update_code" do
+        if ng_conf.include?(:secondcoder)
+          standup_response = open("http://secondcoder.com/api/netguru/#{application}/check").read
+          raise "Computer says no!\n#{standup_response}" unless standup_response == "OK"
+        end
+      end
+
+
+      after "deploy:restart" do
+        if ng_conf.include?(:notify_airbrake)
+          run "cd #{current_path} && #{runner} rake airbrake:deploy TO=#{stage} REVISION=#{current_revision} REPO=#{repository}"
+        end
+      end
+      
     end
   end
+end
 
 
-  after "deploy:restart" do
-    if ng_conf.include?(:notify_airbrake)
-      run "cd #{current_path} && #{runner} rake airbrake:deploy TO=#{stage} REVISION=#{current_revision} REPO=#{repository}"
-    end
-  end
-
+if Capistrano::Configuration.instance
+  Netguru::Capistrano.load_into(Capistrano::Configuration.instance)
 end
---
+++ spec/lib/capistrano_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+require 'capistrano/configuration'
+require 'netguru/capistrano'
+
+describe Netguru::Capistrano do
+  before do
+    @configuration = Capistrano::Configuration.new
+    Netguru::Capistrano.load_into(@configuration)
+    @configuration.dry_run = true
+  end
+
+  it "should define ng_conf" do
+    @configuration.fetch(:ng_conf).should_not be_nil
+  end
+
+  # should "define airbrake:deploy task" do
+  #   assert_not_nil @configuration.find_task('airbrake:deploy')
+  # end
+
+  # should "log when calling airbrake:deploy task" do
+  #   @configuration.set(:current_revision, '084505b1c0e0bcf1526e673bb6ac99fbcb18aecc')
+  #   @configuration.set(:repository, 'repository')
+  #   io = StringIO.new
+  #   logger = Capistrano::Logger.new(:output => io)
+  #   logger.level = Capistrano::Logger::MAX_LEVEL
+    
+  #   @configuration.logger = logger
+  #   @configuration.find_and_execute_task('airbrake:deploy')
+    
+  #   assert io.string.include?('** Notifying Airbrake of Deploy')
+  #   assert io.string.include?('** Airbrake Notification Complete')
+  # end
+end
