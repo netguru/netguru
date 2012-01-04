debf620d4c498c04837a30a177c2540976ad540d
Marcin Stecki <madsheeppl@gmail.com>
2012-01-04 16:42:01 +0100

    middleware

--- lib/netguru/middleware.rb
+++ lib/netguru/middleware.rb
@@ -5,41 +5,45 @@ class Netguru::Middleware
   end
   
   def call(env)
-    status, headers, response = @app.call(env)
-    [status, headers, [response.body.gsub("</body>", "#{secondcoder_response}</body>")]]
+    if defined?(Rails) and Rails.env.development?
+      status, headers, response = @app.call(env)
+      [status, headers, [response.body.gsub("</body>", "#{secondcoder_response}</body>")]]
+    else
+      @app.call(env)
+    end
   end
 
   def application_name
-  	#TO DO: better way?
-  	Rails.application.class.to_s.split("::")[0].downcase
+    #TO DO: better way?
+    Rails.application.class.to_s.split("::")[0].downcase
   end
 
   def secondcoder_response
-  	%{
-  		<div id='secondcoder'>
- 		#{open("http://secondcoder.com/api/netguru/#{application_name}/check").read}
- 		</div>
- 		#{styles}
-  	}
+    %{
+      <div id='secondcoder'>
+    #{open("http://secondcoder.com/api/netguru/#{application_name}/check").read}
+    </div>
+    #{styles}
+    }
    end
 
   def styles
-  	%{	
-	  	<style>
-  		#secondcoder {
-  			position: fixed;
-  			top: 10px;
-  			right: 10px;
-  			color: #990000;
-  			background-color: white;
-  			padding: 2px 5px;
-  			font-size: 12px;
-  			border-radius: 5px;
-  			font-face: "Helvetica Neue",Helvetica,Arial,sans-serif";
-  			border: 2px solid #990000;
-  		}
-  		</style>
-  	}
+    %{  
+      <style>
+      #secondcoder {
+        position: fixed;
+        top: 10px;
+        right: 10px;
+        color: #990000;
+        background-color: white;
+        padding: 2px 5px;
+        font-size: 12px;
+        border-radius: 5px;
+        font-face: "Helvetica Neue",Helvetica,Arial,sans-serif";
+        border: 2px solid #990000;
+      }
+      </style>
+    }
   end
 
 
