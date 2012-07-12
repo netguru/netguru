74ce369668372221d246bac8ac5fe49b4c261757
Adam Nowak <adam.nowak@netguru.pl>
2012-07-12 10:13:58 +0200

    Fixed chmod for git-hooks generator

--- lib/generators/netguru/git_hooks/git_hooks_generator.rb
+++ lib/generators/netguru/git_hooks/git_hooks_generator.rb
@@ -3,13 +3,13 @@ class Netguru::GitHooksGenerator < Rails::Generators::Base
 
   def install_hooks
     install_post_merge_hook
-    chmod '.git/hooks/*', 0755
   end
 
   private
 
   def install_post_merge_hook
     template 'post-merge.sh', '.git/hooks/post-merge'
+    chmod '.git/hooks/post-merge', 0755
   end
 
 end
