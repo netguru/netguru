842e10adb9ad00c7e66a29e0fbf64a4908774f0a
Marcin Stecki <madsheeppl@gmail.com>
2012-08-11 14:16:46 +0200

    respect your elders

--- lib/netguru.rb
+++ lib/netguru.rb
@@ -1,6 +1,6 @@
 require "netguru/version"
 require "netguru/capistrano"
-require "netguru/railtie" if defined? Rails
+require "netguru/railtie" if defined?(Rails) and Rails.version >= '3'
 require "netguru/middleware/block"
 
 module Netguru
