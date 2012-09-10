c740bc2468a4cc15a02d058d753b7025ded4aa39
Adam Nowak <adam.nowak@netguru.pl>
2012-09-10 10:25:18 +0200

    bullet as railtie

--- lib/netguru/railtie.rb
+++ lib/netguru/railtie.rb
@@ -6,10 +6,28 @@ module Netguru
   end
 
   class Railtie < Rails::Railtie
+
+    def right_time_for_bullet?
+      Time.now.tuesday? && Time.now.to_date.cweek.modulo(2).zero?
+    end
+
+    def may_bullet_be_initialized?
+      defined?(Bullet) && Rails.env.development? && right_time?
+    end
+
     if Rails.env.development?
       initializer "netguru.insert_middleware" do |app|
         app.config.middleware.use Netguru::Middleware::Review
       end
+
+      if may_bullet_be_initialized?
+        config.after_initialize do
+          Bullet.enable = true
+          Bullet.alert = true
+          Bullet.bullet_logger = true
+        end
+      end
+
     end
   end
 end
