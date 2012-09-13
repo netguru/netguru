9a352960de273889ad06c40ae5462a1675aed109
Marcin Stecki <madsheeppl@gmail.com>
2012-09-13 15:09:30 +0200

    add netguru yml to templates

--- lib/generators/netguru/netguru_generator.rb
+++ lib/generators/netguru/netguru_generator.rb
@@ -27,6 +27,7 @@ class NetguruGenerator < Rails::Generators::Base
   def install_konf
     template 'preinitializer.rb', 'config/preinitializer.rb'
     template 'config.yml', 'config/config.yml'
+    template 'netguru.yml', 'config/netguru.yml'
     template 'sec_config.yml.sample', 'config/sec_config.yml.sample'
     puts "Add require File.expand_path('../preinitializer', __FILE__) to your application.rb"
   end
