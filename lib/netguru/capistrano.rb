edfd861bb6eef36fc3ff1b07c1fbc1cda8c730e0
Marcin Stecki <madsheeppl@gmail.com>
2012-09-14 12:40:47 +0200

    fix response

--- lib/netguru/capistrano.rb
+++ lib/netguru/capistrano.rb
@@ -219,7 +219,7 @@ module Netguru
             end
 
             if standup_response['commits'] and standup_response['commits']['rejected'].to_i > 0
-              raise "[review] Computer says no! \n[review] There are #{standup_response['commits']['rejected']} rejected commits - #{standup_response['commits']['url']}"
+              raise "[review] Computer says no! - There are #{standup_response['commits']['rejected']} rejected commits - #{standup_response['commits']['url']}"
             else
               puts "[review] Pending #{standup_response['commits']['pending']}, passed #{standup_response['commits']['passed']}"
             end
