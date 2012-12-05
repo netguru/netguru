6131bb73adc2aad7f6d534d3990ce63d51505ec8
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-12-05 17:12:15 +0100

    garbage collector generator

--- lib/generators/netguru/spec_speedup/spec_speedup_generator.rb
+++ lib/generators/netguru/spec_speedup/spec_speedup_generator.rb
@@ -3,5 +3,13 @@ class Netguru::SpecSpeedupGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def install
+    install_garbage_collector
+  end
+
+  private
+
+  def install_garbage_collector
+    template 'garbage_collector.rb', 'spec/spec_helper/garbage_collector.rb'
+    puts "Add require 'spec_helper/garbage_collector' to your spec_helper.rb"
   end
 end
