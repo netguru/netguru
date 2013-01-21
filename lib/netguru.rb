0fc3d008070e9b07a12bd10ac2aa20728294dee1
Marcin Stecki <madsheeppl@gmail.com>
2013-01-21 17:37:45 +0100

    something around the api

--- lib/netguru.rb
+++ lib/netguru.rb
@@ -3,6 +3,7 @@ require "netguru/version"
 require "netguru/capistrano"
 require "netguru/railtie" if defined?(Rails) and Rails.version >= '3'
 require "netguru/middleware/block"
+require "netguru/api"
 require 'konf'
 require 'pry'
 module Netguru
---
+++ lib/netguru/api.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+require 'net/https'
+require 'uri'
+
+class Netguru::Api
+
+  #shortcut for posting data over api
+  def self.post(path, data)
+    url = URI.parse("#{Netguru.config.api.url}/#{path}")
+    req = Net::HTTP::Post.new(url.path)
+    data.merge!(token: Netguru.config.api.token)
+    req.set_form_data(data)
+    request(url, req)
+  end
+
+  def self.get(path)
+    url = URI.parse("#{Netguru.config.api.url}/#{path}")
+    req = Net::HTTP::Get.new(url.path)
+    request(url, req)
+  end
+
+  def self.request(url, req)
+    sock = Net::HTTP.new(url.host, url.port)
+    sock.use_ssl = true
+    sock.start {|http| http.request(req) }
+  end
+
+end
