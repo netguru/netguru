26b1b83d8c3a4dd9a14712304120738cf3e30967
Jacek Jakubik <jacek.jakubik@netguru.pl>
2013-02-25 10:50:37 +0100

    init commit

---
+++ diceplus/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diceplus::Application.config.secret_token = '4afb49f87ea2286820a9bde8b4dab45ffa4713b78b13e3cb4db98c4c304aa5f3966b68fc91cd8bda8429776a199688c5d8246fd43c880e2c07d58cd5a593e16e'
