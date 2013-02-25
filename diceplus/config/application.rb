9fa9d760efaad2fb4c93921a5ddd6a7e65f2692e
Jacek Jakubik <jacek.jakubik@netguru.pl>
2013-02-25 10:56:50 +0100

    circle + no AR

--- diceplus/config/application.rb
+++ diceplus/config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
