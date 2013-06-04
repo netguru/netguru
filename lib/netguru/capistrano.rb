823635b71f3aab5f87bbbc4ca0f4affc7343ce6b
Joanna Rajewska <joanna.rajewska@netguru.pl>
2013-06-04 14:41:54 +0200

    fix [#49995915]

--- lib/netguru/capistrano.rb
+++ lib/netguru/capistrano.rb
@@ -198,11 +198,11 @@ module Netguru
           end
           #notify rb
           task :notify_rollbar, :roles => :app do
-            set :revision, `git log -n 1 --pretty=format:"%H"`
-            set :local_user, `whoami`
-            set :rollbar_token, Netguru.config.rollbar.token
-            rails_env = fetch(:rails_env, 'production')
-            if rollbar_token
+            if Netguru.config.rollbar.token
+              set :revision, `git log -n 1 --pretty=format:"%H"`
+              set :local_user, `whoami`
+              set :rollbar_token, Netguru.config.rollbar.token
+              rails_env = fetch(:rails_env, 'production')
               run "curl https://api.rollbar.com/api/1/deploy/ -F access_token=#{rollbar_token} -F environment=#{rails_env} -F revision=#{revision} -F local_username=#{local_user} >/dev/null 2>&1", :once => true
             end
           end
